c791170d944cc57dff2b8c52d7e898831fe00f06
mikekavouras <kavourasm@gmail.com>
2016-12-28 13:22:42 -0600

    first

---
+++ TapDictionary/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapDictionary
+//
+//  Created by Michael Kavouras on 12/28/16.
+//  Copyright © 2016 Mike Kavouras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
